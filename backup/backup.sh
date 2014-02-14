b1b6175161ef1557a2cf1748360e824106bd1754
Matt Wytock <mwytock@gmail.com>
2014-02-14 13:45:07 -0500

    Add partition size to upload parameters

--- backup/backup.sh
+++ backup/backup.sh
@@ -2,19 +2,36 @@
 
 dir=$HOME/shared
 success=$HOME/backup/SUCCESS
+pidfile=$HOME/backup/pid
 upload=$HOME/scratch/backup/upload.py
 
-desc="Backup archive from $(date)"
-
-cd $dir
-files=$(find . -newer $success -type f)
-
-if [ -z "$files" ]; then
-    exit 1
+# Check if backup is already running
+if [ -f $pidfile ]; then
+    pid=$(cat $pidfile)
+    if kill -0 $pid >/dev/null 2>&1; then
+	exit 0
+    fi
 fi
 
-echo "Backing up modified files"
-echo "${files}"
+echo $$ > $pidfile
 
-echo "${files}" | tar c -T - | xz -9 | $upload backup "$desc"
+cd $dir
+files=$(find . -newer $success -type f -printf "%p\t%k\n")
+if [ -z "$files" ]; then
+    exit 0
+fi
+
+size=$(echo "${files}" \
+    | awk -F"	" 'BEGIN { sum=0 } { sum+=$2 } END { print sum }')
+size=$(( $size/1024 ))
+
+part=1
+while (( $size/$part > 10000 )); do
+    part=$(( $part*2 ))
+done
+
+echo "Backing up ${size}M of modified files in ${part}M chunks..."
+echo "${files}" | awk -F"	" '{print $1}' | tar vc -T - | xz -9 \
+    | $upload backup $part "backup $(date)"
+
 touch $success
--- backup/upload.py
+++ backup/upload.py
@@ -4,12 +4,13 @@ import sys
 
 from boto.glacier import layer2
 
-if len(sys.argv) != 3:
-    print >>sys.stderr, "Usage: ./upload.py <vault> <description>"
+if len(sys.argv) != 4:
+    print >>sys.stderr, "Usage: ./upload.py <vault> <partsize> <description>"
     sys.exit(1)
 
 glacier = layer2.Layer2()
 vault = glacier.get_vault(sys.argv[1])
+vault.DefaultPartSize = int(sys.argv[2]) * 1024 * 1024
 archive_id = vault.create_archive_from_file(file_obj=sys.stdin,
-                                            description=sys.argv[2])
+                                            description=sys.argv[3])
 print "Created archive", archive_id
