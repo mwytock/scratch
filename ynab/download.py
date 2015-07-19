6781a518bba26abcdf88bfac1264eb07e979b602
Matt Wytock <mwytock@gmail.com>
2015-07-19 16:46:12 -0700

    add --days flag

--- ynab/download.py
+++ ynab/download.py
@@ -1,11 +1,17 @@
 #!/usr/bin/env python
 
+import argparse
+
 import ofxclient.util
 import ofxclient.config
 
+parser = argparse.ArgumentParser()
+parser.add_argument("--days", default=40, type=int)
+args = parser.parse_args()
+
 def download_ofx():
     config = ofxclient.config.OfxConfig()
-    data = ofxclient.util.combined_download(config.accounts())
+    data = ofxclient.util.combined_download(config.accounts(), days=args.days)
     f = open("data.ofx", "w")
     f.write(data.read())
 
